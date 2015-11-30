89830918b235e2680d3f21af6cce9ef46e7ff450
Daesun Yim <daesun@robinhood.com>
2015-11-29 18:06:47 -0800

    Create a basic page for IPOs

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
+  test "Should get Home" do
     get :home
     assert_response :success
     assert_select "title", "Home | Rat's Nest"
+  end
+
+  test "Should get IPOs" do
+    get :ipos
+    assert_response :success
+    assert_select "title", "IPOs | Rat's Nest"
   end
 end
